c9f5598cb4daaba84cf9112207c86b415b65bc13
mrtozn1923 <mrtozn1923@gmail.com>
2021-05-20 11:02:16 +0300

    Lesson29 added

---
+++ 5-Project-Github_Finder/29-Navbar_Component-Default_Props/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Navbar from './Navbar'
+
+export class App extends Component {
+    render() {
+        return (
+            // <Navbar title="Github Finder" icon="fab fa-github"/>
+            <Navbar />
+        )
+    }
+}
+
+export default App
