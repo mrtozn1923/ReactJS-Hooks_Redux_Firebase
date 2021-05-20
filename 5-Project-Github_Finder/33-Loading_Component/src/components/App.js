9880b7ad3776097b81d1d11677b77786de48828d
mrtozn1923 <mrtozn1923@gmail.com>
2021-05-20 18:39:00 +0300

    Lesson33 added

---
+++ 5-Project-Github_Finder/33-Loading_Component/src/components/App.js
@@ -0,0 +1,30 @@
+import React, { Component,Fragment } from 'react'
+import Navbar from './Navbar'
+import Users from './Users'
+import axios from 'axios'
+
+export class App extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            loading:false,
+            users:[]
+        }
+    }
+    componentDidMount(){
+        this.setState({loading:true});
+            axios
+            .get('https://api.github.com/users')
+            .then(res=>this.setState({users:res.data,loading:false}));
+    }
+    render() {
+        return (
+            <Fragment>
+                <Navbar />
+                <Users users={this.state.users} loading={this.state.loading}/>
+            </Fragment>
+        )
+    }
+}
+
+export default App
