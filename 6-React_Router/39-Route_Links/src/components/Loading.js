d01a439325186e27a3025637716b7298ed76f6ca
mrtozn1923 <mrtozn1923@gmail.com>
2021-05-22 13:44:29 +0300

    Lesson39 added

---
+++ 6-React_Router/39-Route_Links/src/components/Loading.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import loading from '../assets/loading.gif'
+
+export class Loading extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <img src={loading} alt="Loading" style={{width:'100px',display:'block',margin:'auto'}}/>
+            </React.Fragment>
+        )
+    }
+}
+export default Loading
