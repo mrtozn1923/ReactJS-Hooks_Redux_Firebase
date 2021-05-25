1f566d893d6ee81d8005a2d655576a0255b6703b
mrtozn1923 <mrtozn1923@gmail.com>
2021-05-25 16:58:22 +0300

    Lesson57 added

---
+++ 9-Project-Blog_App/57-React_Router/blog-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<AppRouter />,document.getElementById('root'));
+
+
+reportWebVitals();
