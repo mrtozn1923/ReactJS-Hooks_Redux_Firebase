3c33ef98f3140987be33298beea3b74c4aaf1d5a
mrtozn1923 <mrtozn1923@gmail.com>
2021-05-27 19:01:35 +0300

    Lesson70 added

---
+++ 11-React_Redux/70-Update_Blog/blog-app/src/examples/redux-counter.js
@@ -0,0 +1,54 @@
+import {createStore} from 'redux'
+
+const initialState={
+    count:0
+}
+//REDUCER
+const counterReducer=(state=initialState,action)=>{
+    switch(action.type){
+        case "INCREMENT":
+            const incrementBy=typeof action.incrementBy==='number'?action.incrementBy:1;
+            return{
+                count:state.count+incrementBy
+            }
+        case "DECREMENT":
+            return{
+                count:state.count-1
+            }
+        case "RESET":
+            return{
+                count:0
+            }
+        default:
+            return state
+    }
+}
+
+//store
+const store=createStore(counterReducer);
+
+// //get state
+// console.log(store.getState());
+
+store.subscribe(()=>{
+    console.log(store.getState());
+})
+
+//increment,decrement,reset
+store.dispatch({
+    type:"INCREMENT",
+    incrementBy:10
+})
+
+store.dispatch({
+    type:"INCREMENT"
+})
+
+store.dispatch({
+    type:"DECREMENT"
+})
+
+store.dispatch({
+    type:"RESET"
+})
+
