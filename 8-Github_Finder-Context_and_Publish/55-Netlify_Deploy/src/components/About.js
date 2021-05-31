df1506c2a93dc7dafd43380d248c99e8ae22ec43
mrtozn1923 <mrtozn1923@gmail.com>
2021-05-31 17:45:50 +0300

    Lesson55 updated

--- 8-Github_Finder-Context_and_Publish/55-Netlify_Deploy/src/components/About.js
+++ 8-Github_Finder-Context_and_Publish/55-Netlify_Deploy/src/components/About.js
@@ -3,7 +3,8 @@ import React from 'react'
 const About=()=>(
     <>
         <div className="container">
-            <h1>About Page</h1>
+            <h1 className="mt-3">Hakkımda</h1>
+            <p>Bu uygulama Mert Özen tarafından React temelli uygulama geliştirme için deneme amaçlı yapılmıştır. Arkaplanda Github API kullanılmaktadır.</p>
         </div>
     </>
 );
